d5efc7ace4f4a56851d2238fe7a04ec0b049a0ab
Berar <george.berar@accenture.com>
2018-06-05 15:26:26 +0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-
+/** COMPONENTS **/
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { CallbackComponent } from './callback/callback.component';
+
+/** SERVICES **/
+import { AuthService } from './auth/auth.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WelcomeComponent,
+    CallbackComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+        {path: 'welcome', component: WelcomeComponent},
+        {path: 'secured/home', component: HomeComponent},
+        {path: 'callback', component: CallbackComponent},
+        {path: '**', redirectTo: '/welcome'}
+    ])
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css']
+})
+export class WelcomeComponent implements OnInit {
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+    
+  login(): void {
+    this.authService.login(); 
+  }
+
+}
